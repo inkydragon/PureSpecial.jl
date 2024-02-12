aba4a9124e5e1369cff5100e4d97a3208e89a963
Documenter.jl <documenter@juliadocs.github.io>
2024-02-12 20:28:13 +0000

    build based on 318a0c7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Scipy4j","category":"page"},{"location":"#Scipy4j","page":"Home","title":"Scipy4j","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Scipy4j.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Scipy4j]","category":"page"}]
+}
