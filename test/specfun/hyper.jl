7f4cfbebec6bd191156cfa0cf11e95f14538d4a6
Chengyu HAN <git@wo-class.cn>
2024-02-12 14:36:32 -0600

    specfun.cgama: add simple tests

--- test/specfun/hyper.jl
+++ test/specfun/hyper.jl
@@ -1,7 +1,25 @@
 # SPDX-License-Identifier: MIT
 
-@testset "cgama" begin
+@testset "_cgama" begin
+    test_z = [
+        1.0 + im,
+        1.0 - im,
+        -1.0 + im,
+        -1.0 - im,
+        
+        -1.0 + 0im,
+        -2.0 + 1im,
+        2.0 + 3im,
+    ]
 
+    for z in test_z,
+        kf in 0:1
+        @testset "cgama($z, $kf)" begin
+            ref = _cgama(z, kf)
+            res = Specfun.cgama(z, kf)
+            @test isapprox(ref, res)
+        end
+    end
 end
 
 @testset "hyp1f1/chgm" begin
