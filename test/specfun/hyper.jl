5afd1185a2934aa7ed635f330821c685a4a0a6b6
Chengyu HAN <git@wo-class.cn>
2024-02-13 20:19:05 -0600

    test: specfun.hyp1f1: use Branch Coverage

--- test/specfun/hyper.jl
+++ test/specfun/hyper.jl
@@ -70,6 +70,106 @@ end
     end
 end
 
+function hyp1f1_test(a, b, z::ComplexF64)
+    @testset "cchg($a, $b, $z)" begin
+        @test isapprox(_cchg(a, b, z), Specfun.cchg(a, b, z))
+    end
+end
+function hyp1f1_test(a, b, x::Float64)
+    @testset "chgm($a, $b, $x)" begin
+        # NOTE: _chgm donot handle spcial inputs
+        # @test isapprox(_chgm(a, b, x), Specfun.chgm(a, b, x))
+        ref = _chgm(a, b, x)
+        res = Specfun.chgm_kernel(a, b, x)
+
+        if isnan(ref)
+            @test isequal(ref, res)
+        else
+            @test isapprox(ref, res)
+        end
+    end
+    z = complex(x)
+    hyp1f1_test(a, b, z)
+end
+
+"""
+Branch Coverage Test Inputs for `chgm`
+"""
+const TEST_HYP1F1_ABX = [
+    # ( a, b, x )
+    
+    #= if isinteger(a) && a < 0.0 =#
+    (-23.0, 1.0, 1.0),
+
+    #= if x < 0.0 =#
+    # T1: x < 0.0
+    (1.0,  2.0, -1.0),
+    (1.0, -2.0, -1.0),
+    # F1: x >= 0.0
+    (1.0,  2.0, 0.0),
+    (1.0, -2.0, 0.0),
+    (1.0,  2.0, 1.0),
+    (1.0, -2.0, 1.0),
+
+    #= if a >= 2.0 =#
+    # T2: a >= 2.0
+    (2.0, 1.0, 1.0),
+    (3.1, 1.0, 1.0),
+    # F2: a < 2.0
+    (1.0, 2.0, 1.0),
+    (-1.0, 2.0, 1.0),
+
+    # NOTE: 
+    #   if x < 0.0:     a -= b
+    #   if a >= 2.0:    a -= Int(a) + 1
+
+    #= if (abs(x) <= 30.0 + abs(b)) || (a < 0.0) =#
+    # T3
+    # T31: (T || F):  (abs(x) <= 30+abs(b)) && a >= 0.0
+    # T311=> x >= 0 && a in [0, 2) && (x < 30+abs(b))
+    (0.0,  0.0, 0.0),
+    (0.0,  1.0, 10.0),
+    (1.9,  1.0, 10.0),
+    (1.9,  1.0, 31.0),
+    # T312=> x < 0 && a < 2 && (abs(x) <= 30+abs(b)) && (b-a) > 0
+    (0.1,  1.0, -10.0),
+    (-0.1, -1.0, -10.0),
+    (-0.1, -1.0, -31.0),
+    # T32: (F || T):  (abs(x) > 30+abs(b)) && a < 0
+    # T321=> x >= 0 && ...
+    (-1.9,  1.0, 42.0),
+    (-1.9, -1.0, 42.0),
+    # T322=> x < 0 && (b-a) < 0 && ...
+    (2.0,  1.0, -42.0),
+    (2.0, -1.0, -42.0),
+
+    # F3: (abs(x) > 30+abs(b)) && a >= 0.0
+    #= if x0 >= 0.0  && F3 =#
+    # T4: x0 >= 0
+    (1.9,  1.0, 42.0),
+    (1.9, -1.0, 42.0),
+    # F4: x0 < 0
+    (1.9,  2.0, -42.0),
+    (-2.1, -2.0, -42.0),
+
+    #= if (n == 0) =#
+    # Always
+
+    #= if (n == 1) =#
+    # T5: nl >= 1 ==> a >= 2.0
+    (31.0, 1.0, 1.0),
+    (31.0, -1.0, 1.0),
+
+    #= if (a0 >= 2.0) =#
+    # Same as T2/F2
+]
+
+@testset "hyp1f1/Branch Coverage" begin
+    for (a,b,x) in TEST_HYP1F1_ABX
+        hyp1f1_test(a, b, x)
+    end
+end
+
 @testset "hyp1f1/cchg" begin
     broken_b = Float64(0xBAD)
     test_abz = [
@@ -112,87 +212,6 @@ end
         @testset "cchg($a, $b, $z)" begin
             ref = _cchg(a, b, z)
             res = Specfun.cchg(a, b, z)
-
-            if isnan(ref)
-                @test isequal(ref, res)
-            else
-                @test isapprox(ref, res)
-            end
-        end
-    end
-end
-
-@testset "hyp1f1/chgm" begin
-    test_abx = [
-        # ( a, b, x )
-
-        #= if x < 0.0 =#
-        # T1: x < 0.0
-        (1.0,  2.0, -1.0),
-        (1.0, -2.0, -1.0),
-        # F1: x >= 0.0
-        (1.0,  2.0, 0.0),
-        (1.0, -2.0, 0.0),
-        (1.0,  2.0, 1.0),
-        (1.0, -2.0, 1.0),
-
-        #= if a >= 2.0 =#
-        # T2: a >= 2.0
-        (2.0, 1.0, 1.0),
-        (3.1, 1.0, 1.0),
-        # F2: a < 2.0
-        (1.0, 2.0, 1.0),
-        (-1.0, 2.0, 1.0),
-
-        # NOTE: 
-        #   if x < 0.0:     a -= b
-        #   if a >= 2.0:    a -= Int(a) + 1
-
-        #= if (abs(x) <= 30.0 + abs(b)) || (a < 0.0) =#
-        # T3
-        # T31: (T || F):  (abs(x) <= 30+abs(b)) && a >= 0.0
-        # T311=> x >= 0 && a in [0, 2) && (x < 30+abs(b))
-        (0.0,  0.0, 0.0),
-        (0.0,  1.0, 10.0),
-        (1.9,  1.0, 10.0),
-        (1.9,  1.0, 31.0),
-        # T312=> x < 0 && a < 2 && (abs(x) <= 30+abs(b)) && (b-a) > 0
-        (0.1,  1.0, -10.0),
-        (-0.1, -1.0, -10.0),
-        (-0.1, -1.0, -31.0),
-        # T32: (F || T):  (abs(x) > 30+abs(b)) && a < 0
-        # T321=> x >= 0 && ...
-        (-1.9,  1.0, 42.0),
-        (-1.9, -1.0, 42.0),
-        # T322=> x < 0 && (b-a) < 0 && ...
-        (2.0,  1.0, -42.0),
-        (2.0, -1.0, -42.0),
-
-        # F3: (abs(x) > 30+abs(b)) && a >= 0.0
-        #= if x0 >= 0.0  && F3 =#
-        # T4: x0 >= 0
-        (1.9,  1.0, 42.0),
-        (1.9, -1.0, 42.0),
-        # F4: x0 < 0
-        (1.9,  2.0, -42.0),
-        (-2.1, -2.0, -42.0),
-
-        #= if (n == 0) =#
-        # Always
-
-        #= if (n == 1) =#
-        # T5: nl >= 1 ==> a >= 2.0
-        (31.0, 1.0, 1.0),
-        (31.0, -1.0, 1.0),
-
-        #= if (a0 >= 2.0) =#
-        # Same as T2/F2
-    ]
-
-    for (a,b,x) in test_abx
-        @testset "chgm($a, $b, $x)" begin
-            ref = _chgm(a, b, x)
-            res = Specfun.chgm_kernel(a, b, x)
 
             if isnan(ref)
                 @test isequal(ref, res)
