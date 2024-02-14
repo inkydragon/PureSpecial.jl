e0dd0a8151d4ef455d85ab5e70461f9ebfd72aa4
Chengyu HAN <git@wo-class.cn>
2024-02-13 21:07:20 -0600

    Scipy4j: add scipy.special TOC

--- src/Scipy4j.jl
+++ src/Scipy4j.jl
@@ -1,4 +1,38 @@
 # SPDX-License-Identifier: MIT
+"""
+
+## Special functions
+> ref: [scipy.special - SciPy Manual](https://docs.scipy.org/doc/scipy/reference/special.html)
+
+**TOC**
+- Airy functions
+- Elliptic functions
+- Bessel functions
+    + Zeros of Bessel functions
+    + Faster Common Bessel functions
+    + Integrals of Bessel functions
+    + Derivatives of Bessel functions
+    + Spherical Bessel functions
+    + Riccati-Bessel functions
+- Struve functions
+- Statistical functions: Skip!
+- Gamma functions
+- Error function
+- Fresnel integrals
+- Legendre functions
+- Ellipsoidal harmonics
+- Orthogonal polynomials
+    + evaluate values
+    + roots
+- Hypergeometric functions
+- Parabolic Cylinder functions
+- Mathieu functions
+- Spheroidal Wave functions
+- Kelvin functions
+- Combinatorics
+- Lambert W functions
+- Other Special functions
+"""
 module Scipy4j
 
 include("specfun/Specfun.jl")
