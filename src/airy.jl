9e78f5942901a4d77f5e67c57cb5db519bf5fb1a
Chengyu HAN <git@wo-class.cn>
2024-02-17 14:09:54 -0600

    specfun.airy: use spaces for indent

--- src/airy.jl
+++ src/airy.jl
@@ -3,20 +3,20 @@
 
 """Airy functions
 airy(z)
-	Airy functions and their derivatives.
-	+ airy_wrap -> cephes_airy
-	+ cairy_wrap -> amos_airy,amos_biry
+    Airy functions and their derivatives.
+    + airy_wrap -> cephes_airy
+    + cairy_wrap -> amos_airy,amos_biry
 
 airye(z)
-	Exponentially scaled Airy functions and their derivatives.
-	+ cairy_wrap_e -> amos_airy,amos_biry
-	+ cairy_wrap_e_real -> amos_airy,amos_biry
+    Exponentially scaled Airy functions and their derivatives.
+    + cairy_wrap_e -> amos_airy,amos_biry
+    + cairy_wrap_e_real -> amos_airy,amos_biry
 
 ai_zeros(nt)
-	+ _specfun.airyzo(nt, 1) -> specfun_airyzo
+    + _specfun.airyzo(nt, 1) -> specfun_airyzo
 
 bi_zeros(nt)
-	+ _specfun.airyzo(nt, 2) -> specfun_airyzo
+    + _specfun.airyzo(nt, 2) -> specfun_airyzo
 
 itairy(x)
     + itairy_wrap -> specfun_itairy
@@ -26,43 +26,43 @@ export ai_zeros, bi_zeros, itairy
 
 
 """
-	ai_zeros(nt)
+    ai_zeros(nt)
 
 Compute the first `nt` zeros of Airy functions `Ai(x)` and `Ai'(x)`, `a` and `a'`,
 and the associated values of `Ai(a')` and `Ai'(a)`.
 """
 function ai_zeros(nt)
-	if nt < 0
-		throw(DomainError(nt, "`nt` must be a positive integer (nt >= 0)"))
-	end
+    if nt < 0
+        throw(DomainError(nt, "`nt` must be a positive integer (nt >= 0)"))
+    end
 
-	# for Ai(x) and Ai'(x)
-	kf = 1
-	a, b, c, d = zeros(nt), zeros(nt), zeros(nt), zeros(nt)
+    # for Ai(x) and Ai'(x)
+    kf = 1
+    a, b, c, d = zeros(nt), zeros(nt), zeros(nt), zeros(nt)
 
-	Specfun.airyzo!(nt, kf, a, b, c, d)
+    Specfun.airyzo!(nt, kf, a, b, c, d)
 
-	a, b, c, d
+    a, b, c, d
 end
 
 """
-	bi_zeros(nt)
+    bi_zeros(nt)
 
 Compute the first `nt` zeros of Airy functions `Bi(x)` and `Bi'(x)`, `b` and `b'`,
 and the associated values of `Bi(b')` and `Bi'(b)`.
 """
 function bi_zeros(nt)
-	if nt < 0
-		throw(DomainError(nt, "`nt` must be a positive integer (nt >= 0)"))
-	end
+    if nt < 0
+        throw(DomainError(nt, "`nt` must be a positive integer (nt >= 0)"))
+    end
 
-	# for Bi(x) and Bi'(x)
-	kf = 2
-	a, b, c, d = zeros(nt), zeros(nt), zeros(nt), zeros(nt)
+    # for Bi(x) and Bi'(x)
+    kf = 2
+    a, b, c, d = zeros(nt), zeros(nt), zeros(nt), zeros(nt)
 
-	Specfun.airyzo!(nt, kf, a, b, c, d)
+    Specfun.airyzo!(nt, kf, a, b, c, d)
 
-	a, b, c, d
+    a, b, c, d
 end
 
 """
@@ -72,8 +72,8 @@ Compute the integrals of Airy functions with respect to t from 0 and x ( x ≥ 0
 """
 function itairy(x)
     if x < 0
-		throw(DomainError(x, "`x` must be a positive number (x >= 0)"))
-	end
+        throw(DomainError(x, "`x` must be a positive number (x >= 0)"))
+    end
 
     Specfun.itairy(x)
 end
