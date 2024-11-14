d96ed42fe2171a51ce5ba958c5fea42c4a5207c2
Chengyu HAN <cyhan.dev@outlook.com>
2024-11-14 21:14:03 +0800

    ScipySpecial: clean doc

--- src/ScipySpecial.jl
+++ src/ScipySpecial.jl
@@ -4,11 +4,8 @@
 
 """Elementary Functions"""
 #=
-Lambert W
-
 lambertw(z)
     + lambertw_scalar -> special::lambertw
-        
 wrightomega(z)
     + wrightomega -> wright::wrightomega
     + wrightomega_real -> wright::wrightomega_real
@@ -17,98 +14,43 @@ wrightomega(z)
 
 """Gamma Functions"""
 #=
-
 ## cephes
-gamma(z)
-    gamma function.
-    + cephes_Gamma,cgamma
+gamma(z)            + cephes_Gamma,cgamma
+gammaln(x)          + cephes_lgam
+loggamma(z)         + loggamma_real,loggamma -> special::loggamma -> cephes::lgam
+gammasgn(x)         + cephes_gammasgn
+gammainc(a, x)      + cephes_igam
+gammaincinv(a, y)   + cephes_igami
+gammaincc(a, x)     + cephes_igamc
+gammainccinv(a, y)  + cephes_igamci
 
-gammaln(x)
-    Logarithm of the absolute value of the gamma function.
-    + cephes_lgam
-
-loggamma(z)
-    Principal branch of the logarithm of the gamma function.
-    + loggamma_real,loggamma -> special::loggamma -> cephes::lgam
-
-gammasgn(x)
-    Sign of the gamma function.
-    + cephes_gammasgn
-
-gammainc(a, x)
-    Regularized lower incomplete gamma function.
-    + cephes_igam
-
-gammaincinv(a, y)
-    Inverse to the regularized lower incomplete gamma function.
-    + cephes_igami
-
-gammaincc(a, x)
-    Regularized upper incomplete gamma function.
-    + cephes_igamc
-
-gammainccinv(a, y)
-    Inverse of the regularized upper incomplete gamma function.
-    + cephes_igamci
-
-beta(a, b)
-    Beta function.
-    + cephes_beta
-
-betaln(a, b)
-    Natural logarithm of absolute value of beta function.
-    + cephes_lbeta
+beta(a, b)      + cephes_beta
+betaln(a, b)    + cephes_lbeta
 
 
 ## boost
-betainc(a, b, x)
-    Regularized incomplete beta function.
-    + ibeta_float,ibeta_double @ boost
-
-betaincc(a, b, x)
-    Complement of the regularized incomplete beta function.
-    + ibetac_float,ibetac_double @ boost
-
-betaincinv(a, b, y)
-    Inverse of the regularized incomplete beta function.
-    + ibeta_inv_float,ibeta_inv_double @ boost
-
-betainccinv(a, b, y)
-    Inverse of the complemented regularized incomplete beta function.
-    + ibetac_inv_float,ibetac_inv_double @ boost
+betainc(a, b, x)        + ibeta_float,ibeta_double @ boost
+betaincc(a, b, x)       + ibetac_float,ibetac_double @ boost
+betaincinv(a, b, y)     + ibeta_inv_float,ibeta_inv_double @ boost
+betainccinv(a, b, y)    + ibetac_inv_float,ibetac_inv_double @ boost
 
 
-psi(z)
-    The digamma function.
-    + digamma,cdigamma -> special::digamma
-
-digamma(z)
-    The digamma function.
-    + -> psi
-    
+psi(z)      + digamma,cdigamma -> special::digamma
+digamma(z)  + -> psi
 rgamma(z)
-    Reciprocal of the gamma function.
     + cephes_rgamma
     + crgamma -> special::rgamma
-
-polygamma(n, x)
-    Polygamma functions.
-    + gamma,zeta,psi
-
-multigammaln(a, d)
-    Returns the log of multivariate gamma, also sometimes called the generalized gamma.
-    + loggam
-
-poch(z, m)
-    Pochhammer symbol.
-    + cephes_poch
+polygamma(n, x)     + gamma,zeta,psi
+multigammaln(a, d)  + loggam
+poch(z, m)          + cephes_poch
 =#
 
 
 """Exponential and Trigonometric Integrals"""
 #=
-- exp1(z)
-- expi(x)
+exp1(z)
+expi(x)
+expn(n, x)
 =#
 
 
@@ -116,57 +58,34 @@ poch(z, m)
 #=
 ## cephes,faddeeva
 erf(z)
-    Returns the error function of complex argument.
     + cephes_erf
     + faddeeva_erf
-
 erfc(x)
-    Complementary error function, 1 - erf(x).
     + cephes_erfc
     + faddeeva_erfc_complex
-
 erfcx(x)
-    Scaled complementary error function, exp(x**2) * erfc(x).
     + faddeeva_erfcx
     + faddeeva_erfcx_complex
-
 erfi(z)
-    Imaginary error function, -i erf(i z).
     + faddeeva_erfi
     + faddeeva_erfi_complex
-
 erfinv(y)
-    Inverse of the error function.
     + erfinv_float,erfinv_double @ boost
-
 erfcinv(y)
-    Inverse of the complementary error function.
     + cephes_erfcinv
-
 wofz(z)
-    Faddeeva function
     + faddeeva_w
-
 dawsn(x)
-    Dawson's integral.
     + faddeeva_dawsn
     + faddeeva_dawsn_complex
-
 fresnel(z)
-    Fresnel integrals.
     + cephes_fresnl
     + cfresnl_wrap -> specfun_cfs,specfun_cfc
-
 modfresnelp(x)
-    Modified Fresnel positive integrals
     + modified_fresnel_plus_wrap -> specfun_ffk(ks=0, ...)
-
 modfresnelm(x)
-    Modified Fresnel negative integrals
     + modified_fresnel_minus_wrap -> specfun_ffk(ks=1, ...)
-
 voigt_profile(x, sigma, gamma)
-    Voigt profile.
     + faddeeva_voigt_profile
 
 
@@ -192,80 +111,36 @@ itairy(x)
 #=
 Bessel functions
 jv(v, z)
-    Bessel function of the first kind of real order and complex argument.
     + cbesj_wrap_real -> cbesj_wrap,cephes_jv
     + cbesj_wrap -> amos_besj,amos_besy
         + cbesj_wrap_e
-
-jve(v, z)
-    Exponentially scaled Bessel function of the first kind of order v.
-    + cbesj_wrap_e_real -> cbesj_wrap_e -> amos_besj,amos_besy
-
-yn(n, x)
-    Bessel function of the second kind of integer order and real argument.
-    + cephes_yn
-
+jve(v, z)   + cbesj_wrap_e_real -> cbesj_wrap_e -> amos_besj,amos_besy
+yn(n, x)    + cephes_yn
 yv(v, z)
-    Bessel function of the second kind of real order and complex argument.
     + cbesy_wrap_real -> cbesy_wrap,cephes_yv
     + cbesy_wrap -> amos_besy,amos_besj
-
-yve(v, z)
-    Exponentially scaled Bessel function of the second kind of real order.
-    + cbesy_wrap_e_real -> cbesy_wrap_e -> amos_besy,amos_besj
+yve(v, z)   + cbesy_wrap_e_real -> cbesy_wrap_e -> amos_besy,amos_besj
 
 
 ## amos_besk,amos_besi
-kn(n, x)
-    Modified Bessel function of the second kind of integer order n
-    + cbesk_wrap_real_int -> cbesk_wrap_real -> cbesk_wrap -> amos_besk
-
-kv(v, z)
-    Modified Bessel function of the second kind of real order v
-    + cbesk_wrap_real -> cbesk_wrap -> amos_besk
-
-kve(v, z)
-    Exponentially scaled modified Bessel function of the second kind.
-    + cbesk_wrap_e_real -> cbesk_wrap_e -> amos_besk
-
+kn(n, x)    + cbesk_wrap_real_int -> cbesk_wrap_real -> cbesk_wrap -> amos_besk
+kv(v, z)    + cbesk_wrap_real -> cbesk_wrap -> amos_besk
+kve(v, z)   + cbesk_wrap_e_real -> cbesk_wrap_e -> amos_besk
 iv(v, z)
-    Modified Bessel function of the first kind of real order.
     + cbesi_wrap -> amos_besi,cbesi_wrap_e,amos_besk
         + cbesi_wrap_e -> amos_besi,amos_besk
     + cephes_iv
-
-ive(v, z)
-    Exponentially scaled modified Bessel function of the first kind.
-    + cbesi_wrap_e_real -> cbesi_wrap_e -> amos_besi,amos_besk
+ive(v, z)   + cbesi_wrap_e_real -> cbesi_wrap_e -> amos_besi,amos_besk
 
 
 ## amos_besh
-hankel1(v, z)
-    Hankel function of the first kind
-    + cbesh_wrap1 -> amos_besh
+hankel1(v, z) + cbesh_wrap1 -> amos_besh
+hankel1e(v, z) + cbesh_wrap1_e -> amos_besh
+hankel2(v, z) + cbesh_wrap2 -> amos_besh
+hankel2e(v, z) + cbesh_wrap2_e -> amos_besh
 
-hankel1e(v, z)
-    Exponentially scaled Hankel function of the first kind
-    + cbesh_wrap1_e -> amos_besh
-
-hankel2(v, z)
-    Hankel function of the second kind
-    + cbesh_wrap2 -> amos_besh
-
-hankel2e(v, z)
-    Exponentially scaled Hankel function of the second kind
-    + cbesh_wrap2_e -> amos_besh
-
-    
-wright_bessel(a, b, x)
-    Wright's generalized Bessel function.
-    + wright_bessel_scalar@_wright_bessel.pxd
-
-
-lmbda(v, x)
-    Jahnke-Emden Lambda function, Lambdav(x).
-    + _specfun.lamv -> specfun_lamv
-
+wright_bessel(a, b, x)  + wright_bessel_scalar@_wright_bessel.pxd
+lmbda(v, x)     + _specfun.lamv -> specfun_lamv
 
 
 # Zeros of Bessel functions
@@ -288,138 +163,61 @@ y1p_zeros(nt)
 
 # Faster Common Bessel functions
 
-j0(x)
-    Bessel function of the first kind of order 0.
-    + cephes_j0
-
-j1(x)
-    Bessel function of the first kind of order 1.
-    + cephes_j1
-
-y0(x)
-    Bessel function of the second kind of order 0.
-    + cephes_y0
-
-y1(x)
-    Bessel function of the second kind of order 1.
-    + cephes_y1
-
-i0(x)
-    Modified Bessel function of order 0.
-    + cephes_i0
-
-i0e(x)
-    Exponentially scaled modified Bessel function of order 0.
-    + cephes_i0e
-
-i1(x)
-    Modified Bessel function of order 1.
-    + cephes_i1
-
-i1e(x)
-    Exponentially scaled modified Bessel function of order 1.
-    + cephes_i1e
-
-k0(x)
-    Modified Bessel function of the second kind of order 0, K0(x)
-    + cephes_k0
-
-k0e(x)
-    Exponentially scaled modified Bessel function K of order 0
-    + cephes_k0e
-
-k1(x)
-    Modified Bessel function of the second kind of order 1, K1(x)
-    + cephes_k1
-    
-k1e(x)
-    Exponentially scaled modified Bessel function K of order 1
-    + cephes_k1e
+j0(x)   + cephes_j0
+j1(x)   + cephes_j1
+y0(x)   + cephes_y0
+y1(x)   + cephes_y1
+i0(x)   + cephes_i0
+i0e(x)  + cephes_i0e
+i1(x)   + cephes_i1
+i1e(x)  + cephes_i1e
+k0(x)   + cephes_k0
+k0e(x)  + cephes_k0e
+k1(x)   + cephes_k1
+k1e(x)  + cephes_k1e
 
 
 # Integrals of Bessel functions
-itj0y0(x)
-    Integrals of Bessel functions of the first kind of order 0.
-    + it1j0y0_wrap -> specfun_itjya
+itj0y0(x)   + it1j0y0_wrap -> specfun_itjya
+it2j0y0(x)  + it2j0y0_wrap -> specfun_ittjya
+iti0k0(x)   + it1i0k0_wrap -> specfun_itika
+it2i0k0(x)  + it2i0k0_wrap -> specfun_ittika
 
-it2j0y0(x)
-    Integrals related to Bessel functions of the first kind of order 0.
-    + it2j0y0_wrap -> specfun_ittjya
-
-iti0k0(x)
-    Integrals of modified Bessel functions of order 0.
-    + it1i0k0_wrap -> specfun_itika
-
-it2i0k0(x)
-    Integrals related to modified Bessel functions of order 0.
-    + it2i0k0_wrap -> specfun_ittika
-
-besselpoly(a, lmb, nu)
-    Weighted integral of the Bessel function of the first kind.
-    + cephes_besselpoly
+besselpoly(a, lmb, nu)  + cephes_besselpoly
 
 
 # Derivatives of Bessel functions
 
-jvp(v, z)
-Compute derivatives of Bessel functions of the first kind.
-+ jv(v, z),_bessel_diff_formula
-
-yvp(v, z)
-Compute derivatives of Bessel functions of the second kind.
-+ yv(v, z),_bessel_diff_formula
-
-kvp(v, z)
-Compute derivatives of real-order modified Bessel function Kv(z)
-+ kv(v, z),_bessel_diff_formula
-
-ivp(v, z)
-Compute derivatives of modified Bessel functions of the first kind.
-+ iv(v, z),_bessel_diff_formula
-
-h1vp(v, z)
-Compute derivatives of Hankel function H1v(z) with respect to z.
-+ hankel1(v, z),_bessel_diff_formula
-
-h2vp(v, z)
-Compute derivatives of Hankel function H2v(z) with respect to z.
-+ hankel2(v, z),_bessel_diff_formula
+jvp(v, z)   + jv(v, z),_bessel_diff_formula
+yvp(v, z)   + yv(v, z),_bessel_diff_formula
+kvp(v, z)   + kv(v, z),_bessel_diff_formula
+ivp(v, z)   + iv(v, z),_bessel_diff_formula
+h1vp(v, z)  + hankel1(v, z),_bessel_diff_formula
+h2vp(v, z)  + hankel2(v, z),_bessel_diff_formula
 
 
 # Spherical Bessel functions
 
 spherical_jn(n, z)
-Spherical Bessel function of the first kind or its derivative.
 + _spherical_jn,_spherical_jn_d
     + spherical_jn_real,spherical_jn_complex -> cbesj
-
 spherical_yn(n, z)
-Spherical Bessel function of the second kind or its derivative.
 + _spherical_yn,_spherical_yn_d
     + spherical_yn_d_real -> spherical_yn_real
     + spherical_yn_d_complex -> spherical_yn_complex -> cbesy
-    
 spherical_in(n, z)
-Modified spherical Bessel function of the first kind or its derivative.
 + _spherical_in,_spherical_in_d
     + spherical_in_real -> iv
     + spherical_in_complex -> cbesi_wrap
-
 spherical_kn(n, z)
-Modified spherical Bessel function of the second kind or its derivative.
 + _spherical_kn,_spherical_kn_d
     + spherical_yn_d_real -> spherical_yn_real
     + spherical_yn_d_complex -> spherical_yn_complex -> cbesy
 
 
 # Riccati-Bessel functions
-riccati_jn(n, x)
-Compute Ricatti-Bessel function of the first kind and its derivative.
-+ _specfun.rctj -> specfun_rctj
-
-riccati_yn(n, x)
-Compute Ricatti-Bessel function of the second kind and its derivative.
-+ _specfun.rcty -> specfun_rcty
+riccati_jn(n, x) + _specfun.rctj -> specfun_rctj
+riccati_yn(n, x) + _specfun.rcty -> specfun_rcty
 
 
 # Kelvin functions
@@ -506,57 +304,26 @@ lqmn(m, n, z)
 """Elliptic Integrals and Functions"""
 #=
 ## cephes
-ellipj(u, m)
-    Jacobian elliptic functions
-    + cephes_ellpj
+ellipj(u, m)        + cephes_ellpj
+ellipk(m)           + ellipk -> ellpk -> cephes_ellpk
+ellipkm1(p)         + cephes_ellpk
+ellipkinc(phi, m)   + cephes_ellik
+ellipe(m)           + cephes_ellpe
+ellipeinc(phi, m)   + cephes_ellie
 
-ellipk(m)
-    Complete elliptic integral of the first kind.
-    + ellipk -> ellpk -> cephes_ellpk
-
-ellipkm1(p)
-    Complete elliptic integral of the first kind around m = 1
-    + cephes_ellpk
-
-ellipkinc(phi, m)
-    Incomplete elliptic integral of the first kind
-    + cephes_ellik
-
-ellipe(m)
-    Complete elliptic integral of the second kind
-    + cephes_ellpe
-
-ellipeinc(phi, m)
-    Incomplete elliptic integral of the second kind
-    + cephes_ellie
-
-    
 ## ellint_carlson
-elliprc(x, y)
-    Degenerate symmetric elliptic integral.
-    + fellint_RC,cellint_RC
-
-elliprd(x, y, z)
-    Symmetric elliptic integral of the second kind.
-    + fellint_RD,cellint_RD
-
-elliprf(x, y, z)
-    Completely-symmetric elliptic integral of the first kind.
-    + fellint_RF,cellint_RF
-
-elliprg(x, y, z)
-    Completely-symmetric elliptic integral of the second kind.
-    + fellint_RG,cellint_RG
-
-elliprj(x, y, z, p)
-    Symmetric elliptic integral of the third kind.
-    + fellint_RJ,cellint_RJ
+elliprc(x, y)       + fellint_RC,cellint_RC
+elliprd(x, y, z)    + fellint_RD,cellint_RD
+elliprf(x, y, z)    + fellint_RF,cellint_RF
+elliprg(x, y, z)    + fellint_RG,cellint_RG
+elliprj(x, y, z, p) + fellint_RJ,cellint_RJ
 =#
 
 
 """Zeta Functions"""
 #=
-
+zeta(x[, q])    -> xsf::cephes
+zetac(x)        -> xsf::cephes
 =#
 
 
