53ff600ddda0e26483688224d08d2ba0fa753743
Chengyu HAN <cyhan.dev@outlook.com>
2024-11-14 11:46:06 +0800

    specfun.legendre: init lpmns, lqmns, lpmv

--- src/specfun/legendre.jl
+++ src/specfun/legendre.jl
@@ -21,10 +21,99 @@ Legendre Function
 Associated Legendre Function
 - lpmn:     Associated legendre function, Pmn(x), Pmn'(x)
 - clpmn:    Associated legendre function, Pmn(z), Pmn'(z)
-- lpmns:    Associated legendre function for a given order, Pmn(x), Pmn'(x) 
+- 🚧 lpmns:    Associated legendre function for a given order, Pmn(x), Pmn'(x) 
 - lqmn:     Associated legendre function, Qmn(x), Qmn'(x)
 - clqmn:    Associated legendre function, Qmn(z), Qmn'(z)
-- lqmns:    Associated legendre function for a given order, Qmn(x), Qmn'(x)
-- lpmv:     Associated legendre function, Pmv(x), |x| <= 1, v isa Real, v >= 0, 
+- 🚧 lqmns:    Associated legendre function for a given order, Qmn(x), Qmn'(x)
+- 🚧 lpmv:     Associated legendre function, Pmv(x), |x| <= 1, v isa Real, v >= 0, 
 """
 
+"""
+Compute associated Legendre functions Pmn(x)
+and Pmn'(x) for a given order
+
+Input :
+- x --- Argument of Pmn(x)
+- m --- Order of Pmn(x),  m = 0,1,2,...,n
+- n --- Degree of Pmn(x), n = 0,1,2,...,N
+
+Output:
+- PM(n) --- Pmn(x)
+- PD(n) --- Pmn'(x)
+"""
+function lpmns()
+
+end
+
+"""
+Compute associated Legendre functions Qmn(x)
+and Qmn'(x) for a given order
+
+Input :  
+- x --- Argument of Qmn(x)
+- m --- Order of Qmn(x),  m = 0,1,2,...
+- n --- Degree of Qmn(x), n = 0,1,2,...
+
+Output:  
+- QM(n) --- Qmn(x)
+- QD(n) --- Qmn'(x)
+"""
+function lqmns()
+
+end
+
+"""
+Compute Psi function
+
+Input :  
+- x  --- Argument of psi(x)
+
+Output:  
+- PS --- psi(x)
+
+TODO: move to gamma.jl
+"""
+function psi()
+
+end
+
+"""
+Compute the associated Legendre function
+Pmv(x) with an integer order and an arbitrary
+nonnegative degree v
+
+Input :  
+- x   --- Argument of Pm(x)  ( -1 ≤ x ≤ 1 )
+- m   --- Order of Pmv(x)
+- v   --- Degree of Pmv(x)
+
+Output:  
+- PMV --- Pmv(x)
+
+Routine called:  
+- PSI for computing Psi function
+"""
+function lpmv0()
+
+end
+
+"""
+Compute the associated Legendre function
+Pmv(x) with an integer order and an arbitrary
+degree v, using recursion for large degrees
+
+Input :  
+- x   --- Argument of Pm(x)  ( -1 ≤ x ≤ 1 )
+- m   --- Order of Pmv(x)
+- v   --- Degree of Pmv(x)
+
+Output:  
+- PMV --- Pmv(x)
+
+Routine called:  
+- LPMV0
+- GAMMA2
+"""
+function lpmv()
+
+end
