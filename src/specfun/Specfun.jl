92b02240715aff8a03ad8dbae1af0e01e9b274bf
Chengyu HAN <cyhan.dev@outlook.com>
2024-11-18 16:40:51 +0800

    specfun: update include seq

--- src/specfun/Specfun.jl
+++ src/specfun/Specfun.jl
@@ -15,28 +15,38 @@ C       Copyrighted but permission granted to use code in programs.
 C       Buy their book "Computation of Special Functions", 1996, John Wiley & Sons, Inc.
 C
 ```
-
-
-
 """
 module Specfun
 
 include("const.jl")
 
+## 1~4
+# 1. Bernoulli and Euler Numbers
+# 2. Orthogonal Polynomials
 include("gamma.jl")
-include("exp.jl")
-include("error.jl")
-include("airy.jl")
-
-include("bessel_zeros.jl")
-include("kelvin.jl")
-include("struve.jl")
-include("parabolic.jl")
-include("hyper.jl")
 include("legendre.jl")
 
+## 5~7
+# 5 Bessel Functions
+include("bessel_zeros.jl")
+# 6 Modified Bessel Functions
+# 7 Integrals of Bessel Functions
+
+## 8~12
+include("kelvin.jl")
+include("airy.jl")
+include("struve.jl")
+include("hyper.jl")
+
+## 13~16
+include("parabolic.jl")
 include("mathieu.jl")
 include("spheroidal.jl")
+include("error.jl")
+
+## 17~19
 include("trig_int.jl")
+# Elliptic Integrals and Jacobian Elliptic Functions
+include("exp.jl")
 
 end # PureSpecial
--- test/specfun/Specfun.jl
+++ test/specfun/Specfun.jl
@@ -4,19 +4,31 @@ using PureSpecial.Specfun
 # for Test
 include("warp.jl")
 
-# SUBROUTINE
+## 1~4
+# 1. Bernoulli and Euler Numbers
+# 2. Orthogonal Polynomials
 include("gamma.jl")
-include("exp.jl")
-include("error.jl")
-include("airy.jl")
-
-include("bessel_zeros.jl")
-include("kelvin.jl")
-include("struve.jl")
-include("parabolic.jl")
-include("hyper.jl")
 include("legendre.jl")
 
+## 5~7
+# 5 Bessel Functions
+include("bessel_zeros.jl")
+# 6 Modified Bessel Functions
+# 7 Integrals of Bessel Functions
+
+## 8~12
+include("kelvin.jl")
+include("airy.jl")
+include("struve.jl")
+include("hyper.jl")
+
+## 13~16
+include("parabolic.jl")
 include("mathieu.jl")
 include("spheroidal.jl")
+include("error.jl")
+
+## 17~19
 include("trig_int.jl")
+# Elliptic Integrals and Jacobian Elliptic Functions
+include("exp.jl")
