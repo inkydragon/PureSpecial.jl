e562d8801cd5a38877666f407e9e440612c148df
Chengyu HAN <cyhan.dev@outlook.com>
2024-11-21 18:26:53 +0800

    test.wrap: add chgu

--- test/specfun/warp.jl
+++ test/specfun/warp.jl
@@ -575,6 +575,18 @@ function _chguit(a::Float64, b::Float64, x::Float64)
     hu[], id[]
 end
 
+"""SUBROUTINE CHGU(A,B,X,  HU,MD,ISFER)"""
+function _chgu(a::Float64, b::Float64, x::Float64)
+    hu = Ref{Float64}(NaN)
+    md, isfer = Ref{Int32}(-1), Ref{Int32}(-1)
+    ccall(f77func(:chgu), Cvoid,
+        (Ref{Float64}, Ref{Float64}, Ref{Float64},
+         Ref{Float64}, Ref{Int32}, Ref{Int32}),
+        a, b, x,
+        hu, md, isfer)
+    hu[], md[], isfer[]
+end
+
 """13 Parabolic Cylinder Functions"""
 #=
 - pbwa
