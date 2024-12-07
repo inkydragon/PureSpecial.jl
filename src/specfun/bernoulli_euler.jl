a66731742463d3550e82a526d084b6e4c7ff5910
Chengyu HAN <cyhan.dev@outlook.com>
2024-12-07 19:26:00 +0800

    specfun.bernoulli_euler: add bernoa

--- src/specfun/bernoulli_euler.jl
+++ src/specfun/bernoulli_euler.jl
@@ -2,11 +2,49 @@
 #   See also: src/specfun/LICENSE.md
 """Bernoulli and Euler Numbers"""
 #=
-    BERNOA, 5
-    BERNOB, 6
+BERNOA, 5
+BERNOB, 6
     EULERA, 8
     EULERB, 9
 =#
+
+function bernoa(n::Int, bn::Vector{Float64})
+    bn[1] = 1.0
+    bn[2] = -0.5
+
+    for m = 2:n
+        s = -(1.0/(m+1) - 0.5)
+        for k = 2:(m-1)
+            r = 1.0
+            for j = 2:k
+                r *= (j + m - k) / j
+            end
+            s -= r * bn[k+1]
+        end
+        bn[m+1] = s
+    end
+
+    for m = 3:2:n
+        bn[m+1] = 0.0
+    end
+
+    return bn
+end
+
+"""
+Compute Bernoulli number Bn
+
+Input :
+- n --- Serial number, n >= 2
+
+Output:
+- BN(n) --- Bn, n = 0..N
+"""
+function bernoa(n::Int)
+    bn = zeros(Float64, n+1)
+    bernoa(n, bn)
+    return bn
+end
 
 function bernob(n::Int, bn::Vector{Float64})
     @assert (n+1) >= 3
