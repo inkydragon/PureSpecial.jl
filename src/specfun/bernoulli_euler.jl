e1556caf50d7a100d390eb386d09b7900e719c54
Chengyu HAN <cyhan.dev@outlook.com>
2024-12-07 18:44:46 +0800

    specfun: init bernoulli_euler.jl

---
+++ src/specfun/bernoulli_euler.jl
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: MIT OR BSD-3-Clause
+#   See also: src/specfun/LICENSE.md
+"""Bernoulli and Euler Numbers"""
+#=
+    BERNOA, 5
+    BERNOB, 6
+    EULERA, 8
+    EULERB, 9
+=#
---
+++ test/specfun/bernoulli_euler.jl
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: MIT
+
+@testset "bernoulli" begin
+
+end
+
+@testset "euler" begin
+
+end
