1af3e0301e8fef5f0da4823cf8015249081b44c5
Chengyu HAN <cyhan.dev@outlook.com>
2024-12-15 21:43:59 +0800

    gamma: public `gamma`

--- src/gamma.jl
+++ src/gamma.jl
@@ -8,3 +8,33 @@
 ## Pochhammer Function
 ## Beta Function
 """
+
+export gamma
+
+"""
+    gamma(z)
+
+Compute the gamma function for complex `z`.
+
+# Examples
+```jldoctest
+julia> [ (i, gamma(i)) for i in 1:5 ]
+5-element Vector{Tuple{Int64, Float64}}:
+ (1, 1.0)
+ (2, 1.0)
+ (3, 2.0)
+ (4, 6.0)
+ (5, 24.0)
+
+julia> gamma(0.5)^2 ≈ π
+true
+
+julia> gamma(4 + 1) == prod(1:4) == factorial(4)
+true
+```
+
+See also: `factorial`, `loggamma`.
+"""
+gamma(x::Float64) = Specfun.gamma2(x)
+gamma(z::ComplexF64) = Specfun.cgama(x, 1)
+gamma(i::Integer) = gamma(float(i))
