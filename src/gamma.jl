6f879bce47edcff73810dac0ca4cea9f8f25e6c0
Chengyu Han <cyhan.dev@outlook.com>
2025-12-14 13:07:51 +0800

    gamma: use `export` to compat julia LTS
    Signed-off-by: Chengyu Han <cyhan.dev@outlook.com>

--- src/gamma.jl
+++ src/gamma.jl
@@ -10,7 +10,7 @@
 """
 
 # Factorial Function
-public factorial, binomial  # Base.factorial, Base.binomial
+export factorial, binomial  # Base.factorial, Base.binomial
 export factorial2, factorialk
 # Gamma Function
 export gamma, loggamma, logabsgamma, digamma, trigamma, polygamma
