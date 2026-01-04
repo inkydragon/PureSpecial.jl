9c12449e095c6857e1c709cebbda630622666c13
Chengyu HAN <cyhan.dev@outlook.com>
2026-01-04 23:27:30 +0800

    doc: add doc for AbstractSpecialFunctions

--- src/api/AbstractSpecialFunctions.jl
+++ src/api/AbstractSpecialFunctions.jl
@@ -1,6 +1,31 @@
 # SPDX-License-Identifier: MIT
 """
 This package provides abstract interfaces for Special Functions.
+
+- Gamma Functions
+- Exponential Integrals
+- Error Functions
+- Airy Functions
+- Bessel Functions
+- Struve Functions
+- Parabolic Cylinders
+- Hypergeometric Functions
+- Legendre Functions
+- 𝑞-Functions
+- Orthogonal Polynomials
+- Elliptic Integrals & Functions
+- Bernoulli and Euler Polynomials
+- Zeta Functions
+- Combinatorial Analysis
+- Number Theory Functions
+- Mathieu Functions
+- Spheroidal Waves
+- Lamé & Heun Functions
+- Coulomb Functions
+
+# Reference
+
+- [DLMF: NIST Digital Library of Mathematical Functions](https://dlmf.nist.gov/)
 """
 module AbstractSpecialFunctions
 
