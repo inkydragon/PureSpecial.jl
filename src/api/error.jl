c61f51416fad95b88fe4ac21ced731d926a7fcfa
Chengyu HAN <cyhan.dev@outlook.com>
2026-01-09 21:38:36 +0800

    doc/api: add spec for Error Functions

--- src/api/error.jl
+++ src/api/error.jl
@@ -3,11 +3,78 @@
 # Error Functions
 
 ## Error Function
+- [`erf`](@ref), [`erfc`](@ref), [`erfi`](@ref)
+- `faddeeva`
+
 ## Dawson Integral
+- [`dawson`](@ref)
+
 ## Fresnel Integral
+- [`fresnelc`](@ref), [`fresnels`](@ref)
+- fresnelf, fresnelg
+
+## Voigt Function
+- voigtu, voigtv
 
 # Reference
 - [DLMF: Chapter 7 Error Functions, Dawson’s and Fresnel Integrals](https://dlmf.nist.gov/7)
 """
 const ErrorFunctionsDoc = nothing
+
+
+"""
+    erf(x::Real)
+    erf(z::Complex)
+
+Error function.
+"""
+function erf end
+
+"""
+    erfc(x::Real)
+    erfc(z::Complex)
+
+Complementary error function.
+"""
+function erfc end
+
+"""
+    erfi(x::Real)
+    erfi(z::Complex)
+
+Imaginary error function.
+"""
+function erfi end
+
+"""
+    faddeeva(x::Real)
+    faddeeva(z::Complex)
+
+Faddeeva function, `w(z)`.
+"""
+function faddeeva end
+
+"""
+    dawson(x::Real)
+    dawson(z::Complex)
+
+Dawson integral.
+"""
+function dawson end
+
+"""
+    fresnelc(x::Real)
+    fresnelc(z::Complex)
+
+Fresnel integral, `C(z)`.
+"""
+function fresnelc end
+
+"""
+    fresnels(x::Real)
+    fresnels(z::Complex)
+
+Fresnel integral, `S(z)`.
+"""
+function fresnels end
 
